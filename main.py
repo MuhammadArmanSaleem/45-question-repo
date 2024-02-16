5d4d3e1fea9ebc2809be6f594491bcfb271ab2a0
MuhammadArmanSaleem <armansaleem326@gmail.com>
2024-02-16 23:04:14 +0500

    45 question Sir Zia Khan

---
+++ main.py
@@ -0,0 +1,44 @@
+#Name:Arman Saleem
+#Roll No:00304671
+#City:karachi
+#Days/time: Tuesday - 09:00 AM - 12:00 PM
+#exercise 1
+const = ("Salman")
+print (f"Hello {const}")
+#exercise 2
+const = ("Kamran khan")
+print(const.lower())
+print(const.upper())
+print(const.title())
+#exercise 3
+mashhoorPerson = ("Arman Main")
+kehtayhai = ("gibrish")
+print (f'{mashhoorPerson} aik martaba kaha "{kehtayhai}"')
+#exercise 4
+mashhoorPerson = ("Arman Main")
+kehtayhai = ("gibrish")
+Message = (f'{mashhoorPerson} aik martaba kaha "{kehtayhai}"')
+print(Message)
+#exercise 5 sir nhi ata
+#exercise 6
+#Addition
+print("Add:", 5+3)
+#divide
+print("divide:", 16/2)
+#Minus
+print("Minus:",10-2)
+#Multiply
+print("Multiply",2*4)
+#exercise 7 none.js main hai
+#exercise 8
+pasandeedanumber = (6)
+Message2 =(f"my favorite number is {pasandeedanumber}") 
+print(Message2)
+#exercise 9
+names = "Arman","Kamran","Shaktimaan","Ghufran","Anas"
+print(names)
+#execrcise 10 iski list nhi ban rahi
+names2 ="Arman","Kamran","Shaktimaan","Ghufran","Anas"
+print (f"assalam o alaikum {names2}kia haal hain sab theek")
+#exercise 11 sir nahi ata list banana
+#is kai baad sai poora hi nhi ata :)
---
+++ none.js
@@ -0,0 +1,4 @@
+console.log("add",5+3)
+console.log("divide",16/2)
+console.log("Minus",10-2)
+console.log("Multiply",2*4)
